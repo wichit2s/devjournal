fa28c1dd66e39e73a0c3995ce85f0193e576d392
paul <wichit.s@ubu.ac.th>
2018-04-27 22:37:59 +0700

    try adding toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,6 +16,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+
+    MatToolbarModule,
+
+
     AppRoutingModule
   ],
   providers: [],
